eed83bc0d709d93b9651fdb55afd5e36793a7f10
Miroslav Rusin <miroslav.rusin@hotovo.org>
2014-10-22 16:50:55 +0200

    added test for the agg. strategy

---
+++ src/test/java/org/mule/templates/transformers/SFDCContactsMergeAggregationStrategyTest.java
@@ -0,0 +1,105 @@
+/**
+ * Mule Anypoint Template
+ * Copyright (c) MuleSoft, Inc.
+ * All rights reserved.  http://www.mulesoft.com
+ */
+
+package org.mule.templates.transformers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleEvent;
+import org.mule.api.routing.AggregationContext;
+import org.mule.templates.integration.AbstractTemplateTestCase;
+
+import com.google.common.collect.Lists;
+
+@SuppressWarnings("unchecked")
+@RunWith(MockitoJUnitRunner.class)
+public class SFDCContactsMergeAggregationStrategyTest extends AbstractTemplateTestCase {
+	
+	@Mock
+	private MuleContext muleContext;
+  
+	
+	@Test
+	public void testAggregate() throws Exception {
+		List<Map<String, String>> usersA = createUserLists("A", 0, 1);
+		List<Map<String, String>> usersB = createUserLists("B", 1, 2);
+		
+		MuleEvent testEventA = getTestEvent("");
+		MuleEvent testEventB = getTestEvent("");
+		
+		testEventA.getMessage().setPayload(usersA.iterator());
+		testEventB.getMessage().setPayload(usersB.iterator());
+		
+		List<MuleEvent> testEvents = new ArrayList<MuleEvent>();
+		testEvents.add(testEventA);
+		testEvents.add(testEventB);
+		
+		AggregationContext aggregationContext = new AggregationContext(getTestEvent(""), testEvents);
+		
+		SFDCContactMergeAggregationStrategy sfdcuserMerge = new SFDCContactMergeAggregationStrategy();
+		Iterator<Map<String, String>> iterator = (Iterator<Map<String, String>>) sfdcuserMerge.aggregate(aggregationContext).getMessage().getPayload();
+		List<Map<String, String>> mergedList = Lists.newArrayList(iterator);
+
+		Assert.assertEquals("The merged list obtained is not as expected", createExpectedList(), mergedList);
+
+	}
+
+	private List<Map<String, String>> createExpectedList() {
+		Map<String, String> user0 = new HashMap<String, String>();
+		user0.put("IDInA", "0");
+		user0.put("IDInB", "");
+		user0.put("Email", "some.email.0@fakemail.com");
+		user0.put("Name", "SomeName_0");
+
+		Map<String, String> user1 = new HashMap<String, String>();
+		user1.put("IDInA", "1");
+		user1.put("IDInB", "1");
+		user1.put("Email", "some.email.1@fakemail.com");
+		user1.put("Name", "SomeName_1");
+
+		Map<String, String> user2 = new HashMap<String, String>();
+		user2.put("IDInA", "");
+		user2.put("IDInB", "2");
+		user2.put("Email", "some.email.2@fakemail.com");
+		user2.put("Name", "SomeName_2");
+
+		List<Map<String, String>> userList = new ArrayList<Map<String, String>>();
+		userList.add(user0);
+		userList.add(user1);
+		userList.add(user2);
+
+		return userList;
+
+	}
+
+	private List<Map<String, String>> createUserLists(String orgId, int start, int end) {
+		List<Map<String, String>> userList = new ArrayList<Map<String, String>>();
+		for (int i = start; i <= end; i++) {
+			userList.add(createUser(orgId, i));
+		}
+		return userList;
+	}
+
+	private Map<String, String> createUser(String orgId, int sequence) {
+		Map<String, String> user = new HashMap<String, String>();
+		user.put("Id", new Integer(sequence).toString());
+		user.put("Name", "SomeName_" + sequence);
+		user.put("Username", String.valueOf(sequence));
+		user.put("Email", "some.email." + sequence + "@fakemail.com");
+		return user;
+	}
+}
